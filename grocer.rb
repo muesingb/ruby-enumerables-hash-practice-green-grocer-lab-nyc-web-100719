294d108c954b0688e12873e535db2542d1317422
Barbara Muesing <muesingb@gmail.com>
2019-09-16 16:48:53 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,19 @@
-def consolidate_cart(cart)
-  # code here
+def consolidate_cart(cart) #cart is array [{"AVOCADO" => {:price => 3.00, :clearance => true}}, avocado, tempeh].
+  #cart_hash[key] = value. key is avocado. AVOCADO is cart[0]
+  #{"AVOCADO" => {:price => 3.00, :clearance => true}}
+cart_hash = {}
+cart.each do |item|
+  cart_hash[item.keys[0]] = item.values[0]
+    if cart_hash[item.keys[0]][:count]
+     cart_hash[item.keys[0]][:count] += 1
+    else
+     cart_hash[item.keys[0]][:count] = 1
+    end
+  end
+cart_hash
+
+#cart.reduce({}) do |memo, (key, value)
+#  end
 end
 
 def apply_coupons(cart, coupons)
