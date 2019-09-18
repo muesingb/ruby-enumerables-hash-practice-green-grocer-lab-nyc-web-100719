2f97a9ecc7f9d80c59a9443509508ad79de19498
Barbara Muesing <muesingb@gmail.com>
2019-09-18 19:09:44 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -6,13 +6,6 @@ cart.each do |item|
   cart_hash[item.keys[0]] = item.values[0]
 
 cart_hash[item.keys[0]][:count]? cart_hash[item.keys[0]][:count] += 1 : cart_hash[item.keys[0]][:count] = 1
-
-
-#    if cart_hash[item.keys[0]][:count]
-#     cart_hash[item.keys[0]][:count] += 1
-#    else
-#     cart_hash[item.keys[0]][:count] = 1
-#    end
   end
 cart_hash
 
@@ -21,13 +14,30 @@ cart_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |array|
+    coupon_item = "#{array[:item]}" #get item name out of array
+    cart_item = cart[coupon_item] #get item of same name out of cart
+    coupon_already_applied = cart[(coupon_item + " W/COUPON")] #Has coupon been applied - does name already exist in cart?
+    if cart_item
+      cart[(coupon_item + " W/COUPON")] = {:price => (array[:cost]/array[:num]),
+        :clearance => (cart_item[:clearance]),
+        :count => (if coupon_already_applied
+          coupon_already_applied[:count] + array[:num]
+          else array[:num]
+        end)
+      }
+    cart_item[:count] = (cart_item[:count] - array[:num]) #update count of non-couponed items of same name
+    end
+  end
+  return cart
 end
 
-def apply_clearance(cart)
-  # code here
+def apply_clearance(cart) #discount the price of every item on clearance by 20 percent
+  cart.map { |cart_key, cart_value|
+    cart_value[:clearance]? cart_value[:price] = ((cart_value[:price])*0.8).round(2) : cart_value[:price]}
+  return cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart.consolidate_cart
 end
